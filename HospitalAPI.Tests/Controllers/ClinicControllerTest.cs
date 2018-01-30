0b6876d816a66f5db1cf9bd57dd80fae012527bb
AndrewSamoydyuk <AndrewSamoydyuk@gmail.com>
2018-01-30 10:16:42 +0200

    add appveyor.yml, codecov.yml and simple test

---
+++ HospitalAPI.Tests/Controllers/ClinicControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using HospitalAPI.Controllers;
+using System.Web.Mvc;
+using Xunit;
+using Moq;
+using HospitalAPI.Models;
+using System.Web;
+using HospitalAPI.DALs;
+using HospitalAPI.DTOs;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net;
+using System.Web.Http.Results;
+using System.Web.Http;
+
+namespace HospitalAPI.Tests.Controllers
+{
+    public class ClinicControllerTest
+    {
+        [Fact]
+        public void ClinicNotFoundIsRight()
+        {
+            var mockRepository = new Mock<IClinicRepository>();
+            //mockRepository.Setup(c => c.ClinicDetails(1)).Returns(new ClinicDetailDTO());
+            ClinicsController controller = new ClinicsController(mockRepository.Object);
+            IHttpActionResult result = controller.GetClinic(1);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HospitalAPI
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/cb41ba1fe7814aaba1c770107bd731a7)](https://www.codacy.com/app/AndrewSamoydyuk/HospitalAPI?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=AndrewSamoydyuk/HospitalAPI&amp;utm_campaign=Badge_Grade)
